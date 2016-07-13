38a5e839e26d8c501d8188597d2cd7f13d09d24b
jackalwei <jackal8125@gmail.com>
2016-07-13 10:17:25 +0800

    rspec BankAccount can deposit and withdraw

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,2 +1,16 @@
 class BankAccount < ActiveRecord::Base
+
+  def deposit(num)
+    update_attributes(amount: self.amount + num) if num.positive?
+  end
+
+  def balance
+    self.amount
+  end
+
+  def withdraw(num)
+    if num.positive? && num <= self.amount
+      update_attributes(amount: self.amount - num)
+    end
+  end
 end
--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe BankAccount, type: :model do
+  it "可以存錢" do
+    account = BankAccount.create(name: "kk", amount: 100)
+    account.deposit(100)
+    expect(account.balance).to be 200
+  end
 
+  it "不可以存負的錢" do
+    account = BankAccount.create(name: "kk", amount: 100)
+    account.deposit(-100)
+    expect(account.balance).to be 100
+  end
+
+  it "可以領錢" do
+    account = BankAccount.create(name: "kk", amount: 100)
+    account.withdraw(10)
+    account.withdraw(30)
+    expect(account.balance).to be 60
+  end
+
+  it "不可以領負的錢" do
+    account = BankAccount.create(name: "kk", amount: 100)
+    account.withdraw(-100)
+    expect(account.balance).to be 100
+  end
 end
